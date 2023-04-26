b4139d1f35589f688cd4356c2c474714304ac32b
Moses <t.content012@gmail.com>
2023-04-26 13:42:38 +0300

    first commit

---
+++ src/components/Login.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+      <h1>login</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="password" />
+        </div>
+        <input type="submit" value="submit" />
+      </form>
+    </div>
+  )
+}
+
+export default Login
--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Login from './components/Login'
+import NavBar from './components/NavBar'
 
-function Home() {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-}
-
-function App() {
-  return <Home />;
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <NavBar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/"></Route>
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById('root')
+)
